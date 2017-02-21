e09ef19dff20f4b7d361106cdf7fb90d782b2dff
Maddie <mk186442@falmouth.ac.uk>
2017-02-21 17:01:45 +0000

    Added Fish script

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Fish class  
+ */
+public class Fish : MonoBehaviour {
+    public enum CollisionStates
+    {
+        None,
+        Player,
+        OtherFish,
+        Environment
+    };
+    public CollisionStates collisionState;
+
+    // Use this for initialization
+    void Start () {
+        collisionState = CollisionStates.None;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+            collisionState = CollisionStates.Player;
+        else if (col.gameObject.tag == "Fish")
+            collisionState = CollisionStates.OtherFish;
+        else
+            collisionState = CollisionStates.None;
+    }
+}
