31d8010b9623467617040965c6859261863b4a5f
Maddie <mk186442@falmouth.ac.uk>
2017-02-22 12:30:05 +0000

    Fish can now take damage from predators

---
+++ Assets/Scripts/Leaf Nodes/TakeDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+
+public class TakeDamage : ActionNode
+{
+    public Fish fish;
+    public override Status Update()
+    {
+        if (fish.getHealth() < 1)
+            return Status.Failure;
+        else
+        {
+            int randomDamage = UnityEngine.Random.Range(0, 51);
+            fish.setHealth(fish.getHealth() - 50 - randomDamage);
+            return Status.Success;
+        }
+            
+    }
+}
