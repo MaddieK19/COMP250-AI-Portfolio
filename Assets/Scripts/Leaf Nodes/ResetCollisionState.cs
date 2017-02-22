fbde515dc5d5d04d339c760bf6e9dcaa46e272e4
Maddie <mk186442@falmouth.ac.uk>
2017-02-22 21:56:11 +0000

    Added functions to reset the timer and fish collisionState

---
+++ Assets/Scripts/Leaf Nodes/ResetCollisionState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+public class ResetCollisionState : ActionNode
+{
+    // Fish to see if fish has collided with anything
+    public Fish fish;
+
+    public override Status Update()
+    {
+        fish.collisionState = Fish.CollisionStates.None;
+        return Status.Success;
+    }
+}
---
+++ Assets/Scripts/Leaf Nodes/ResetMovementTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+public class ResetMovementTimer : ActionNode
+{
+    // Fish to see if fish has collided with anything
+    public Fish fish;
+
+    public override Status Update()
+    {
+        fish.resetMovementTimer();
+        return Status.Success;
+    }
+}
