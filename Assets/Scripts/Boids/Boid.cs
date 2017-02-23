05388c81bac72ded9c8fdec6e03a63269a2d3c4d
Maddie <mk186442@falmouth.ac.uk>
2017-02-23 16:38:17 +0000

    Replaced fish with boid

---
+++ Assets/Scripts/Boids/Boid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boid : MonoBehaviour {
+    // Vector3 for the boid velocity
+    Vector3 velocity;
+
+    // int for the fishes health level
+    private int health = 100;
+
+    // Enum of possible collision statuses for the boid
+    public enum CollisionStates
+    {
+        None,
+        Player,
+        OtherFish,
+        Predator,
+        Environment
+    };
+    // The boid current collision status
+    public CollisionStates collisionState;
+
+    // returns the boids health 
+    public int getHealth()
+    {
+        return health;
+    }
+
+    // sets the boids health
+    public void setHealth(int newHealth)
+    {
+        health = newHealth;
+    }
+
+    // Use this for initialization
+    void Start () {
+        collisionState = CollisionStates.None;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        //if (col.gameObject.tag == "Player")
+        //   collisionState = CollisionStates.Player;
+        if (col.gameObject.tag == "Fish")
+            collisionState = CollisionStates.OtherFish;
+        else if (col.gameObject.tag == "Predator")
+            collisionState = CollisionStates.Predator;
+        else
+            collisionState = CollisionStates.None;
+    }
+}
--- Assets/Scripts/Boids/BoidController.cs
+++ Assets/Scripts/Boids/BoidController.cs
@@ -2,24 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoidController : MonoBehaviour {
+/*
+ *  Using pseudocode from http://www.kfish.org/boids/pseudocode.html
+ */
 
+public class BoidController : MonoBehaviour {
+    // boid prefab
     public GameObject boid;
-    public Vector3 flockCenter;
+    // Vec3 for the center of the flocking
+    public Vector3 flockCenter = new Vector3(0,0,0);
+    // the maximum number of boids that will be spawned
     static int maxBoids = 5;
+    //! int for the range of coordinates where boids can spawn
+    int spawnArea = 3;
+    // float for the boids movement speed
+    float speed = 0.6f;
+    //! Array of boid prefabs
     public GameObject[] boids = new GameObject[maxBoids];
+
+
+    //Vector 3 used to determine boids next position 
+    Vector3 alignmentVector, cohesionVector, separationVector;
+
 
     // Use this for initialization
     void Start () {
+        // Fills the boids array with boid prefabs
 		for (int i = 0; i < maxBoids; i++)
         {
-            Vector3 boidPosition = new Vector3(i, Random.Range(1, 20), Random.Range(1, 5));
+            Vector3 boidPosition = new Vector3(Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea));
             boids[i] = Instantiate(boid, boidPosition, Quaternion.identity) as GameObject;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       for (int i = 0; i < maxBoids; i++)
+        {
+            alignmentVector = align();
+            cohesionVector = cohesion();
+            separationVector = seperate();
+
+            boids[i].velocity
+            //boids[i].transform.Translate(0, 0, Time.deltaTime * speed);
+            //boids[i].transform.position = Vector3.MoveTowards(boids[i].transform.position, move.transform.position, speed * Time.deltaTime);
+        }
+
+    }
 }
