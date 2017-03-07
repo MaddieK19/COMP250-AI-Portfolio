e7499a3a26908c284f7c04553bb0ea32719c1421
Maddie <mk186442@falmouth.ac.uk>
2017-03-07 11:44:06 +0000

    When not flocking boids will swim off in random directions

---
+++ Assets/Scripts/Leaf Nodes/GenerateRunAwayDirections.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+using System;
+
+public class GenerateRunAwayDirections : ActionNode
+{
+    public BoidController controller;
+    public override Status Update()
+    {
+        controller.chooseFleeDirection();
+        return Status.Success;
+    }
+}
